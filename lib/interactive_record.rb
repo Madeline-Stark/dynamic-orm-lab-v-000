5d2b4d0f1d9abfd2d2d1f86d2b5d731c4826a30c
Madeline Stark <madeline.stark1@gmail.com>
2018-08-17 22:03:32 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,8 +54,8 @@ class InteractiveRecord
   end
 
   def self.find_by({attribute: value})
-    sql = "SELECT * FROM #{self.table_name} WHERE ? = ?"
-    DB[:conn].execute(sql, attribute:, value)
+    sql = "SELECT * FROM #{self.table_name} WHERE '#{attribute:}' = '#{value}'"
+    DB[:conn].execute(sql)
   end
 
 end
