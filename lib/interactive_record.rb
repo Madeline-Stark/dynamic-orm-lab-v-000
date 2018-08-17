85c0538fa6374d0228fac936afc46b733baf9034
Madeline Stark <madeline.stark1@gmail.com>
2018-08-17 21:59:34 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,4 +53,9 @@ class InteractiveRecord
     DB[:conn].execute(sql)
   end
 
+  def self.find_by(attribute: value)
+    sql = "SELECT * FROM #{self.table_name} WHERE ? = ?"
+    DB[:conn].execute(sql, attribute:, value)
+  end 
+
 end
